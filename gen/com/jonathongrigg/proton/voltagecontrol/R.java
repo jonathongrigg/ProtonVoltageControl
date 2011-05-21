3a743f6eedaa62cb287993d119e0ec73217fe97d
Jonathon Grigg <jonathongrigg@gmail.com>
2011-05-21 20:21:55 +0800

    Improved UI

--- gen/com/jonathongrigg/proton/voltagecontrol/R.java
+++ gen/com/jonathongrigg/proton/voltagecontrol/R.java
@@ -14,11 +14,12 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050001;
+        public static final int button1=0x7f050004;
         public static final int button2=0x7f050002;
         public static final int button3=0x7f050003;
         public static final int editText1=0x7f050000;
-        public static final int textView1=0x7f050004;
+        public static final int linearLayout1=0x7f050001;
+        public static final int textView1=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
