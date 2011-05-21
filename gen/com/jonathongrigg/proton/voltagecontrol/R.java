b3f5739aad4b7594be2283bf348af26c5b50ea2c
Jonathon Grigg <jonathongrigg@gmail.com>
2011-05-21 21:34:09 +0800

    Save settings on boot and basic error checking

--- gen/com/jonathongrigg/proton/voltagecontrol/R.java
+++ gen/com/jonathongrigg/proton/voltagecontrol/R.java
@@ -17,6 +17,7 @@ public final class R {
         public static final int button1=0x7f050004;
         public static final int button2=0x7f050002;
         public static final int button3=0x7f050003;
+        public static final int checkBox1=0x7f050005;
         public static final int editText1=0x7f050000;
         public static final int linearLayout1=0x7f050001;
     }
--- src/com/jonathongrigg/proton/voltagecontrol/VoltageControl.java
+++ src/com/jonathongrigg/proton/voltagecontrol/VoltageControl.java
@@ -8,11 +8,14 @@ package com.jonathongrigg.proton.voltagecontrol;
 ** 	http://www.opensource.org/licenses/osl-3.0
 */
 
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -29,11 +32,23 @@ public class VoltageControl extends Activity {
     	Button applyVoltagesButton = (Button) findViewById(R.id.button1);
     	Button existingVoltagesButton = (Button) findViewById(R.id.button2);
     	Button defaultVoltagesButton = (Button) findViewById(R.id.button3);
+    	final CheckBox saveOnBootCheckBox = (CheckBox) findViewById(R.id.checkBox1);
     	
         applyVoltagesButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-        		ShellInterface.runCommand(buildUvCommand(newVoltages));
-        		Toast.makeText(getBaseContext(), "Voltages Applied Successfully", Toast.LENGTH_SHORT).show();
+            	if (saveOnBootCheckBox.isChecked()) {
+            		ShellInterface.runCommand(buildUvCommand(newVoltages));
+            		saveBootSettings(newVoltages);
+            	}
+            	else {
+            		if (newVoltages.length() > 0) {
+            		ShellInterface.runCommand(buildUvCommand(newVoltages));
+            		Toast.makeText(getBaseContext(), "Voltages Applied Successfully", Toast.LENGTH_SHORT).show();
+            		}
+            		else {
+            			Toast.makeText(getBaseContext(), "ERROR: No voltages entered!", Toast.LENGTH_LONG).show();
+            		}
+            	}
             }
         });
     	
@@ -75,5 +90,29 @@ public class VoltageControl extends Activity {
 		command.append("\" > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table");//
 	
 		return command.toString();
+	}
+	
+	private void saveBootSettings(EditText et) {
+		try {
+			OutputStreamWriter out = new OutputStreamWriter(openFileOutput(
+					"proton_voltage_control", 0));
+			String tmp = "#!/system/bin/sh\n\n"
+					+ buildUvCommand(et);
+			out.write(tmp);
+			out.close();
+		} catch (java.io.IOException e) {
+			Toast.makeText(this, "ERROR: file not saved!", Toast.LENGTH_LONG)
+					.show();
+		}
+
+		ShellInterface
+				.runCommand("chmod 777 /data/data/com.jonathongrigg.proton.voltagecontrol/files/proton_voltage_control");
+		ShellInterface.runCommand("busybox mount -o remount,rw  /system");
+		ShellInterface
+				.runCommand("busybox cp /data/data/com.jonathongrigg.proton.voltagecontrol/files/S_volt_scheduler /etc/init.d/proton_voltage_control");
+		ShellInterface.runCommand("busybox mount -o remount,ro  /system");
+		Toast.makeText(this,
+				"Settings saved in file \"/etc/init.d/proton_voltage_control\"",
+				Toast.LENGTH_LONG).show();
 	}
 }
