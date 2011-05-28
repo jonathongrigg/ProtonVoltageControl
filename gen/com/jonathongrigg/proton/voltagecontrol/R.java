e1e20736973e633b1c16056f68e6b01208a52f9d
Jonathon Grigg <jonathongrigg@gmail.com>
2011-05-28 10:20:46 +0800

    Pull through preference changes. Eclipse build files...

--- gen/com/jonathongrigg/proton/voltagecontrol/R.java
+++ gen/com/jonathongrigg/proton/voltagecontrol/R.java
@@ -8,6 +8,9 @@
 package com.jonathongrigg.proton.voltagecontrol;
 
 public final class R {
+    public static final class array {
+        public static final int theme_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class color {
@@ -23,33 +26,38 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int LinearLayout02=0x7f070001;
-        public static final int ScrollView01=0x7f070000;
-        public static final int TextView01=0x7f070011;
-        public static final int button1=0x7f07000f;
-        public static final int button2=0x7f070003;
-        public static final int button3=0x7f070005;
-        public static final int button4=0x7f070004;
-        public static final int button5=0x7f070010;
-        public static final int checkBox1=0x7f07000e;
-        public static final int editText100=0x7f070006;
-        public static final int editText1000=0x7f07000a;
-        public static final int editText1200=0x7f07000b;
-        public static final int editText1300=0x7f07000c;
-        public static final int editText1400=0x7f07000d;
-        public static final int editText200=0x7f070007;
-        public static final int editText400=0x7f070008;
-        public static final int editText800=0x7f070009;
-        public static final int linearLayout1=0x7f070002;
-        public static final int menuEmergencyBoot=0x7f070013;
-        public static final int menuSettings=0x7f070012;
+        public static final int LinearLayout02=0x7f080001;
+        public static final int ScrollView01=0x7f080000;
+        public static final int button1=0x7f08000f;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080005;
+        public static final int button4=0x7f080004;
+        public static final int button5=0x7f080010;
+        public static final int checkBox1=0x7f08000e;
+        public static final int editText100=0x7f080006;
+        public static final int editText1000=0x7f08000a;
+        public static final int editText1200=0x7f08000b;
+        public static final int editText1300=0x7f08000c;
+        public static final int editText1400=0x7f08000d;
+        public static final int editText200=0x7f080007;
+        public static final int editText400=0x7f080008;
+        public static final int editText800=0x7f080009;
+        public static final int linearLayout1=0x7f080002;
+        public static final int menuEmergencyBoot=0x7f080015;
+        public static final int menuSettings=0x7f080014;
+        public static final int saveSettings=0x7f080013;
+        public static final int theme=0x7f080011;
+        public static final int themeSpinner=0x7f080012;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int settings=0x7f030001;
+        public static final int main_proton_theme=0x7f030001;
+        public static final int settings=0x7f030002;
+        public static final int settings_proton_theme=0x7f030003;
+        public static final int spinner_settings=0x7f030004;
     }
     public static final class menu {
-        public static final int menu=0x7f060000;
+        public static final int menu=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
