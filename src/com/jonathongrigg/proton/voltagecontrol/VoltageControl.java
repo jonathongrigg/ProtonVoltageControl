0e7a66bec50e8ed766cbc757ea980bcc82a421b8
Jonathon Grigg <jonathongrigg@gmail.com>
2011-07-10 17:34:14 +0800

    Remove VoltageList and merge with oltageControl (testing done)

--- src/com/jonathongrigg/proton/voltagecontrol/VoltageControl.java
+++ src/com/jonathongrigg/proton/voltagecontrol/VoltageControl.java
@@ -12,6 +12,7 @@ import java.io.OutputStreamWriter;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -24,7 +25,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class VoltageControl extends Activity {
+public class VoltageControl extends ListActivity {
 	// user entered custom voltages 100mhz-1.4ghz
 	int seekBar100;
 	int seekBar200;
